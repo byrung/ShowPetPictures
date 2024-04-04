78127f4089a9e45e7f6170fd790d49210a328b70
byrung <kl78411@naver.com>
2024-04-04 16:27:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/showpetpictures/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.showpetpictures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+
+class MainActivity : AppCompatActivity() {
+    lateinit var checkStart : CheckBox
+    lateinit var rg : RadioGroup
+    lateinit var linearSub : LinearLayout
+    lateinit var btnDone : Button
+    lateinit var imgV : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkStart = findViewById<CheckBox>(R.id.checkStart)
+        rg = findViewById<RadioGroup>(R.id.rg)
+        linearSub = findViewById<LinearLayout>(R.id.linerSub)
+        btnDone = findViewById<Button>(R.id.btnDone)
+        imgV = findViewById<ImageView>(R.id.imgV)
+
+        checkStart.setOnCheckedChangeListener { buttonView, isChecked ->
+         if(isChecked){
+            linearSub.visibility= View.VISIBLE
+         }else{
+             linearSub.visibility=View.INVISIBLE
+         }
+        }
+
+        btnDone.setOnClickListener {
+            when(rg.checkedRadioButtonId){
+                R.id.Dog -> imgV.setImageResource(R.drawable.Dog)
+                R.id.Cat -> imgV.setImageResource(R.drawable.Cat)
+                R.id.Bird -> imgV.setImageResource(R.drawable.Bird)
+            }
+        }
+    }
+}
